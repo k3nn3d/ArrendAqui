03a0c203174fa0b8e018faeb5a27d06f22e6aace
k3nn3d <k3nn3dycorreia@gmail.com>
2023-04-15 17:28:54 +0100

    Adição do DomPDF

--- resources/views/admin/pdf/pdf.blade.php
+++ resources/views/admin/pdf/pdf.blade.php
@@ -1,1 +1,22 @@
-use Barryvdh\DomPDF\Facade\Pdf;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>use Barryvdh\DomPDF\Facade\Pdf;</title>
+</head>
+<body>
+    
+
+<h1>
+    
+</h1>
+
+<script src="{{ asset('./js/geolocalizacao.js')}}"></script>
+</body>
+</html>
+
+
+
+
